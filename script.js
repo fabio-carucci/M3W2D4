17bd9dcd17bd3e79bad23721775570943c2f8fdc
Fabio Carucci <garu2000@live.it>
2024-01-04 21:00:00 +0100

    point 8 completed

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+//Set the starting point of the main content based on the variable height of the fixed-position navbar
+document.querySelector('main').style.marginTop = document.querySelector('nav.navbar').offsetHeight + 20 + 'px';
 
+//Function that brings the window to the navbar link, preventing it from ending up below it.
+//It remove the class "show" from the .navbar-collapse when a nav-link is clicked.
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        document.querySelector('.navbar-collapse').classList.remove('show');
+
+        let linkId = link.getAttribute('href').substring(1);
+        let linkElement = document.getElementById(linkId);
+
+        window.scrollTo(0, linkElement.offsetTop - document.querySelector('nav.navbar').offsetHeight);
+    });
+});
